ddeb5068a3df66822a31d9b47d5439393b5edd54
Wilber Javier Ormeño Vera <wilber.ormeno@hotmail.com>
2018-04-12 20:07:51 -0500

    added login & cabecera

---
+++ cabecera.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span>Cerrar sesión</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+</head>
+</html>
---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <?php
+    include("cabecera.php")
+
+    ?>
+    <meta charset="UTF-8">
+    <title>Busqueda General</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <script src="js/jquery-1.11.3.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/sRegistro.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <style>
+        #msform {
+            max-width: 400px;
+            padding: 15px;
+            margin: 0 auto;
+        }
+        #msform input[type=text]:focus + i.fa {
+            opacity: 1;
+            left: 30px;
+            transition: all 0.25s ease-out;
+        }
+        #msform input[type=password]:focus + i.fa {
+            opacity: 1;
+            left: 30px;
+            transition: all 0.25s ease-out;
+        }
+
+        div.container {
+            top: 200px;
+            position: relative }
+        div.container label {
+            margin: 0;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            margin-right: -50%;
+            transform: translate(-50%, -50%) }
+    </style>
+
+
+</head>
+
+<body style="background-color: lightgrey">
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <label style="font-family: 'Tw Cen MT'; font-weight: bold; color: red; font-size: 50px ;">CETAPSI</label>
+        </div>
+    </div>
+    <br><br><br>
+    <form id="msform" action="autenticacion.php" method="POST">
+        <input type="text" id="textuser" name="user" placeholder="Usuario" autofocus required/>
+        <i class="fa fa-user"></i>
+        <input type="password" id="textpass" name="pass" id="myPassword" placeholder="Contraseña" required/>
+        <i class="fa fa-key"></i>
+        <br>
+        <button id="login-password" type="submit" class="btn btn-lg" style="background-color: black; color: white;" onclick="buscarUsuario()">Ingresar</button>
+    </form>
+</div>
+
+<script src="js/jquery-1.11.3.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/fRegistro.js"></script>
+</body>
+</html>
